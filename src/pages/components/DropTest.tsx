d8e37a8ccbd84d942f739d963ed93edb0e60e2ce
kainholy <ookuraseiya0506@gmail.com>
2024-09-27 11:16:15 +0900

    シフト作成画面 更新

--- src/pages/components/DropTest.tsx
+++ src/pages/components/DropTest.tsx
@@ -118,7 +118,36 @@ const DroppableAreaProject = ({ id, items, project, isActive }) => {
         <Heading fontSize='md'>/</Heading>
         <Heading fontSize='md'>{project.name}</Heading>
       </Flex>
-      <Text fontSize='sm'>Tel: {project.tel}</Text>
+      <Flex gap='8px' align='flex-end'>
+        <Text fontSize='sm'>{project.managerName}</Text>
+        <Text fontSize='sm'>{project.managerTel}</Text>
+      </Flex>
+      <Flex mt='8px' gap='20px' align='center'>
+        <Text fontSize='sm'>🕰️</Text>
+        <Flex gap='8px' align='center'>
+          <Text fontSize='sm'>{project.startTime} ~ {project.endTime}</Text>
+          （
+          <Badge variant='outline' colorScheme='orange' p='0 5px'>
+            <Text p='1px 7px'>日勤(平日)</Text>
+          </Badge>
+          ）
+        </Flex>
+      </Flex>
+      <Flex gap='20px' align='center'>
+        <Text fontSize='sm'>👮‍♀️</Text>
+        <Flex gap='8px' align='center'>
+          <Text fontSize='sm'>{project.numberOfStaff}</Text>
+          （
+          <Flex gap='4px' align='center'>
+            {project.requiredQualifications.map((qualification) => (
+              <Badge key={qualification.name} variant='outline' colorScheme='blue' p='0 5px'>
+                <Text p='1px 7px'>{qualification.name} {qualification.number}</Text>
+              </Badge>
+            ))}
+          </Flex>
+          ）
+        </Flex>
+      </Flex>
       <Flex ref={setNodeRef} gap="12px" mt='24px' wrap='wrap'>
         {items.map((item) => (
           <DraggableCardProject key={item.id} id={item.id} name={item.name} qualification={item.qualification} />
